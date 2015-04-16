ab106796d46070b3da7f213050f104a141be0934
Dilkhush soni <dilkhushsoni2010@gmail.com>
2015-04-16 15:01:42 +0530

    Update ajax_link_renderer.rb

--- lib/will_paginate/view_helpers/ajax_link_renderer.rb
+++ lib/will_paginate/view_helpers/ajax_link_renderer.rb
@@ -1,23 +1,31 @@
-module WillPaginateHelper
-  class AjaxLinkRenderer < WillPaginate::ActionView::LinkRenderer
-    def prepare(collection, options, template)
-      options[:params] ||= {}
-      options[:params]["_"] = nil
-      super(collection, options, template)
-    end
+require 'cgi'
+require 'will_paginate/core_ext'
+require 'will_paginate/view_helpers'
+require 'will_paginate/view_helpers/link_renderer_base'
+require 'will_paginate/view_helpers/link_renderer'
 
-    protected
-    def link(text, target, attributes = {})
-      if target.is_a? Fixnum
-        attributes[:rel] = rel_value(target)
-        target = url(target)
+module WillPaginate
+  module ViewHelpers
+    class AjaxLinkRenderer < LinkRenderer
+      def prepare(collection, options, template)
+        options[:params] ||= {}
+        options[:params]["_"] = nil
+        super(collection, options, template)
       end
-      ajax_call = "$.ajax({url: '#{target}', dataType: 'script'});"
-      @template.link_to_function(text.to_s.html_safe, ajax_call, attributes)
-    end
-  end
 
-  def ajax_will_paginate(collection, options = {})
-    will_paginate(collection, options.merge(:renderer => WillPaginateHelper::AjaxLinkRenderer))
+      protected
+      def link(text, target, attributes = {})
+        if target.is_a? Fixnum
+          attributes[:rel] = rel_value(target)
+          target = url(target)
+        end
+        ajax_call = "$.ajax({url: '#{target}', dataType: 'script'});"
+        @template.link_to_function(text.to_s.html_safe, ajax_call, attributes)
+      end
+    end
+
+    def ajax_will_paginate(collection, options = {})
+      will_paginate(collection, options.merge(:renderer => WillPaginate::ViewHelpers::AjaxLinkRenderer))
+    end
   end
 end
